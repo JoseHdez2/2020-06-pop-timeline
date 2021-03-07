982f0c2cb60b9f5d2fe50350e4e9c0d95029743a
Jose Hernandez <josemanuelhdez2@gmail.com>
2021-03-07 15:35:37 +0000

    reduce redundant data by using map. const groupIds.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Timeline from "react-calendar-timeline";
 // make sure you include the timeline stylesheet or the timeline will not be styled
 import "react-calendar-timeline/lib/Timeline.css";
 import moment from "moment";
-import items from "./items";
+import { groupIds, items } from "./items";
 
 const groups = [
-  { id: 1, title: "Windows" },
-  { id: 2, title: "U.S. President" },
-  { id: 3, title: "Pokemon" },
-  { id: 4, title: "Nintendo console" },
-  { id: 5, title: "Nintendo handheld" },
-  { id: 6, title: "Sony console" },
-  { id: 7, title: "Sony console" }
+  { id: groupIds.windows, title: "Windows" },
+  { id: groupIds.presidents, title: "U.S. President" },
+  { id: groupIds.pkmnGames, title: "Pokemon game" },
+  { id: groupIds.ntdoConsoles, title: "Nintendo console" },
+  { id: groupIds.ntdoHandhelds, title: "Nintendo handheld" },
+  { id: groupIds.sonyConsoles, title: "Sony console" },
+  { id: groupIds.sonyHandhelds, title: "Sony handheld" },
+  { id: groupIds.msftConsoles, title: "Microsoft console" },
+  { id: groupIds.marioGames, title: "Mario game" }
 ];
 
 export default function App() {
@@ -22,8 +24,8 @@ export default function App() {
       <Timeline
         groups={groups}
         items={items}
-        defaultTimeStart={moment().add(-12, "hour")}
-        defaultTimeEnd={moment().add(12, "hour")}
+        defaultTimeStart={moment().add(-10, "year")}
+        defaultTimeEnd={moment()}
       />
     </div>
   );
--- src/items.js
+++ src/items.js
@@ -3,147 +3,119 @@ import randomColor from "randomcolor";
 
 let randomSeed = Math.floor(Math.random() * 1000);
 
-let items = [
+let id = 1;
+
+export const groupIds = {
+  windows: 1,
+  presidents: 2,
+  ntdoConsoles: 3,
+  ntdoHandhelds: 4,
+  sonyConsoles: 5,
+  sonyHandhelds: 6,
+  msftConsoles: 7,
+  pkmnGames: 8,
+  marioGames: 9
+};
+
+const setGroup = (el, i, ary, groupId) =>
+  new Object({
+    id: id++,
+    group: groupId,
+    end_time: i + 1 in ary ? ary[i + 1].start_time : moment(),
+    ...el
+  });
+
+export let items = [
   {
-    id: 1,
-    group: 1,
+    title: "Windows XP",
+    start_time: moment("2001-08")
+  },
+  {
+    title: "Windows 7",
+    start_time: moment("2009-10")
+  },
+  {
+    title: "Windows 8",
+    start_time: moment("2012-10")
+  },
+  {
     title: "Windows 10",
     start_time: moment("2015-07"),
-    end_time: moment(),
     color: randomColor({ luminosity: "light", seed: randomSeed })
-  },
-  {
-    id: 2,
-    group: 1,
-    title: "Windows 8",
-    start_time: moment("2012-10"),
-    end_time: moment("2015-07")
-  },
-  {
-    id: 3,
-    group: 1,
-    title: "Windows 7",
-    start_time: moment("2009-10"),
-    end_time: moment("2012-10")
-  },
-  {
-    id: 4,
-    group: 1,
-    title: "Windows XP",
-    start_time: moment("2001-08"),
-    end_time: moment("2009-10")
   }
-]; // .map((elm, ind) => {id: ind; group: 1; ...elm});
+].map((el, i, ary) => setGroup(el, i, ary, groupIds.windows));
 
 items = [
   ...items,
-  {
-    id: 21,
-    group: 2,
-    title: "Donald Trump",
-    start_time: moment("2016-11"),
-    end_time: moment(),
-    color: "red"
-  },
-  {
-    id: 22,
-    group: 2,
-    title: "Barack Obama",
-    start_time: moment("2008-11"),
-    end_time: moment("2016-11")
-  },
-  {
-    id: 23,
-    group: 2,
-    title: "George W. Bush",
-    start_time: moment("2000-11"),
-    end_time: moment("2008-11")
-  }
+  ...[
+    {
+      title: "George W. Bush",
+      start_time: moment("2000-11")
+    },
+    {
+      title: "Barack Obama",
+      start_time: moment("2008-11")
+    },
+    {
+      title: "Donald Trump",
+      start_time: moment("2016-11"),
+      color: "red"
+    }
+  ].map((el, i, ary) => setGroup(el, i, ary, groupIds.presidents))
 ];
 
 items = [
   ...items,
-  {
-    id: 11,
-    group: 3,
-    title: "Pokemon Red and Blue (US)",
-    start_time: moment("1996-02"),
-    end_time: moment("2000-10")
-  },
-  {
-    id: 12,
-    group: 3,
-    title: "Pokemon Gold and Silver (US)",
-    start_time: moment("2000-10"),
-    end_time: moment("2003-03")
-  },
-  {
-    id: 13,
-    group: 3,
-    title: "Pokemon Ruby and Sapphire (US)",
-    start_time: moment("2003-03"),
-    end_time: moment("2007-04")
-  },
-  {
-    id: 14,
-    group: 3,
-    title: "Pokemon Diamond and Pearl (US)",
-    start_time: moment("2007-04"),
-    end_time: moment("2012-10")
-  }
-]; // .map((elm, ind) => {id: ind; group: 1; ...elm});
+  ...[
+    {
+      title: "Pokemon Red and Blue (US)",
+      start_time: moment("1996-02")
+    },
+    {
+      title: "Pokemon Gold and Silver (US)",
+      start_time: moment("2000-10")
+    },
+    {
+      title: "Pokemon Ruby and Sapphire (US)",
+      start_time: moment("2003-03")
+    },
+    {
+      title: "Pokemon Diamond and Pearl (US)",
+      start_time: moment("2007-04")
+    }
+  ].map((el, i, ary) => setGroup(el, i, ary, groupIds.pkmnGames))
+];
 
 items = [
   ...items,
-  {
-    id: 41,
-    group: 4,
-    title: "NES",
-    start_time: moment("1985-10"),
-    end_time: moment("1991-08")
-  },
-  {
-    id: 42,
-    group: 4,
-    title: "SNES",
-    start_time: moment("1991-08"),
-    end_time: moment("1996-11")
-  },
-  {
-    id: 43,
-    group: 4,
-    title: "N64",
-    start_time: moment("1996-11"),
-    end_time: moment("2001-11")
-  },
-  {
-    id: 44,
-    group: 4,
-    title: "GameCube",
-    start_time: moment("2001-11"),
-    end_time: moment("2006-11")
-  },
-  {
-    id: 45,
-    group: 4,
-    title: "Wii",
-    start_time: moment("2006-11"),
-    end_time: moment("2012-11")
-  },
-  {
-    id: 46,
-    group: 4,
-    title: "Wii U",
-    start_time: moment("2012-11"),
-    end_time: moment("2017-03")
-  },
-  {
-    id: 47,
-    group: 4,
-    title: "Switch",
-    start_time: moment("2017-03"),
-    end_time: moment()
-  }
+  ...[
+    {
+      title: "NES",
+      start_time: moment("1985-10")
+    },
+    {
+      title: "SNES",
+      start_time: moment("1991-08")
+    },
+    {
+      title: "N64",
+      start_time: moment("1996-11")
+    },
+    {
+      title: "GameCube",
+      start_time: moment("2001-11")
+    },
+    {
+      title: "Wii",
+      start_time: moment("2006-11")
+    },
+    {
+      title: "Wii U",
+      start_time: moment("2012-11")
+    },
+    {
+      title: "Switch",
+      start_time: moment("2017-03")
+    }
+  ].map((el, i, ary) => setGroup(el, i, ary, groupIds.ntdoConsoles))
 ];
-
-export default items;
